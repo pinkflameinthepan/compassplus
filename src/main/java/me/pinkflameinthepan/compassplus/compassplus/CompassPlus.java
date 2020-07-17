4229d4de9ab81437cd6f3cea786959a078ed2bcc
pinkflameinthepan <68199238+pinkflameinthepan@users.noreply.github.com>
2020-07-17 10:30:29 +0100

    Initial commit

---
+++ src/main/java/me/pinkflameinthepan/compassplus/compassplus/CompassPlus.java
@@ -0,0 +1,17 @@
+package me.pinkflameinthepan.compassplus.compassplus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CompassPlus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
